1e3948333bf44c19a12a19dfe3788d9bcc131d1e
saba0033 <sekhv23@freeuni.edu.ge>
2025-07-08 19:08:37 +0400

    abstract class and interface for repository, CRUD

---
+++ src/main/Java/repository/AbstractRepository.java
@@ -0,0 +1,19 @@
+package repository;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public abstract class AbstractRepository<GetDto, CreateDto, UpdateDto> implements GenericRepository<GetDto, CreateDto, UpdateDto> {
+    protected BasicDataSource dataSource;
+
+    public AbstractRepository(BasicDataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    protected Connection getConnection() throws SQLException {
+        return dataSource.getConnection();
+    }
+
+}
