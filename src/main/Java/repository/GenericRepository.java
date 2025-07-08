d84b0f4a247ab519cf4cef28e261a369bfee3c44
ochiga07 <aochi23@freeuni.edu.ge>
2025-07-08 22:11:51 +0400

    fixed the return type of create, needed generated_id

--- src/main/Java/repository/GenericRepository.java
+++ src/main/Java/repository/GenericRepository.java
@@ -5,7 +5,7 @@ import java.util.List;
 public interface GenericRepository<GetDto, CreateDto, UpdateDto> {
     GetDto getById(int id);
     List<GetDto> getAll();
-    void create(CreateDto entity);
+    int create(CreateDto entity);
     void update(UpdateDto entity);
     void deleteById(int id);
 }
