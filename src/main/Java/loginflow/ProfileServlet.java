1066bea525b6c44c2e96e8b79d9734263250ebf3
andria <spikygollum@gmail.com>
2025-07-11 23:55:38 +0400

    final

---
+++ src/main/Java/loginflow/ProfileServlet.java
@@ -0,0 +1,29 @@
+package loginflow;
+
+import quiz.QuizRepository;
+import resources.DatabaseConnection;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class ProfileServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String searchedUsername = request.getParameter("user");
+        request.setAttribute("searchedUser", searchedUsername);
+        System.out.println(searchedUsername);
+        QuizRepository quizRepo = new QuizRepository(DatabaseConnection.getDataSource());
+        int takenCount = 0;
+        try {
+            takenCount = quizRepo.getTakenQuizCount(searchedUsername);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        request.setAttribute("quizTakenCount", takenCount);
+        request.getRequestDispatcher("profile.jsp").forward(request, response);
+    }
+}
